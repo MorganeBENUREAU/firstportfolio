bf376c5a2f8232130e030d13eb4fa81ebf47393b
MorganeBENUREAU <morgane.benureau@gmail.com>
2021-09-07 14:53:58 +0200

    modification projets

--- src/components/Projets/index.js
+++ src/components/Projets/index.js
@@ -12,56 +12,56 @@ const Projets = () => (
   <div className="projets">
 
     <h2>Projets</h2>
-
-    <section>
-        <h3> NotaBebe </h3>
-        <div className="container-techno">
-          <img src={nodeJS} alt="nodeJS" className="container-techno__img"/>
-          <img src={postgreSQL} alt="postgresQL" className="container-techno__img"/>
-          <img src={react} alt="react" className="container-techno__img"/>
-        </div>
-      
-        <p className="container__more-informations">
-          Projet réalisé en groupe de 5 (3 personnes côté front, 2 côté back dont  moi-même). Ce projet a été fait dans le cadre de ma formation O'clock comme projet de fin d'étude.
-          <Link exact to="https://notabebe.netlify.app/">lien du projet</Link>
-        </p>
-      </section>
-
     <div className="container">
       <section>
-        <h3> Bibli'O </h3>
-        <div className="container-techno">
-          <img src={nodeJS} alt="nodeJS" className="container-techno__img"/>
-          <img src={postgreSQL} alt="postgresQL" className="container-techno__img"/>
-        </div>
-      
-        <p className="container__more-informations">
-          Api de bibliothèque personnelle faite en NodeJS et PostgreSQL. Début de front en EJS, mon but est de faire le front en React. (en cours)
-        </p>
-      </section>
+          <h3> NotaBebe </h3>
+          <div className="container-techno">
+            <img src={nodeJS} alt="nodeJS" className="container-techno__img"/>
+            <img src={postgreSQL} alt="postgresQL" className="container-techno__img"/>
+            <img src={react} alt="react" className="container-techno__img"/>
+          </div>
+        
+          <p className="container__more-informations">
+            Projet réalisé en groupe de 5 (3 personnes côté front, 2 côté back, dont moi-même). Ce projet a été fait dans le cadre de ma formation O'clock comme projet de fin d'études. C'est un site qui permet de faire le lien entre des parents et une structure d'accueil comme une crèche.
+            <Link exact to="https://notabebe.netlify.app/">lien du projet</Link>
+          </p>
+        </section>
 
       
-      <section>
-        <h3> Projet à venir </h3>
-        {/* <img src={img} alt="projets" className="projets__img"/> */}
-        {/* <p className="container__more-informations">
-          presentation succinte
-        </p> */}
-      </section>
-      {/* <section>
-        <h3> Projet à venir </h3>
-        <img src={img} alt="projets" className="projets__img"/>
-        <p className="container__more-informations">
-          presentation succinte
-        </p>
-      </section> */}
-      {/* <section>
-        <h3> Projet à venir </h3>
-        <img src={img} alt="projets" className="projets__img"/>
-        <p className="container__more-informations">
-          presentation succinte
-        </p>
-      </section> */}
+        <section>
+          <h3> Bibli'O </h3>
+          <div className="container-techno">
+            <img src={nodeJS} alt="nodeJS" className="container-techno__img"/>
+            <img src={postgreSQL} alt="postgresQL" className="container-techno__img"/>
+          </div>
+        
+          <p className="container__more-informations">
+            Api de bibliothèque personnelle faite en NodeJS et PostgreSQL. Début de front en EJS, mon but est de faire le front en React. (en cours)
+          </p>
+        </section>
+
+        
+        <section>
+          <h3> Projet à venir </h3>
+          {/* <img src={img} alt="projets" className="projets__img"/> */}
+          {/* <p className="container__more-informations">
+            presentation succinte
+          </p> */}
+        </section>
+        {/* <section>
+          <h3> Projet à venir </h3>
+          <img src={img} alt="projets" className="projets__img"/>
+          <p className="container__more-informations">
+            presentation succinte
+          </p>
+        </section> */}
+        {/* <section>
+          <h3> Projet à venir </h3>
+          <img src={img} alt="projets" className="projets__img"/>
+          <p className="container__more-informations">
+            presentation succinte
+          </p>
+        </section> */}
     </div>
 
   </div>
