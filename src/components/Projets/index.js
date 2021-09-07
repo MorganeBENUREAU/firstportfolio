d383ddfe18fd9ba444d38927a3d64d6d2a0d7943
MorganeBENUREAU <morgane.benureau@gmail.com>
2021-09-07 14:48:30 +0200

    ajout projet notabebe

--- src/components/Projets/index.js
+++ src/components/Projets/index.js
@@ -1,8 +1,10 @@
 import './style.scss';
 
+import { Link } from 'react-router-dom';
+
 import nodeJS from '../../images/logo-nodeJS.png';
 import postgreSQL from '../../images/logo-postgresql.png';
-// import react from '../../images/logo-react.png';
+import react from '../../images/logo-react.png';
 
 
 const Projets = () => (
@@ -10,6 +12,20 @@ const Projets = () => (
   <div className="projets">
 
     <h2>Projets</h2>
+
+    <section>
+        <h3> NotaBebe </h3>
+        <div className="container-techno">
+          <img src={nodeJS} alt="nodeJS" className="container-techno__img"/>
+          <img src={postgreSQL} alt="postgresQL" className="container-techno__img"/>
+          <img src={react} alt="react" className="container-techno__img"/>
+        </div>
+      
+        <p className="container__more-informations">
+          Projet réalisé en groupe de 5 (3 personnes côté front, 2 côté back dont  moi-même). Ce projet a été fait dans le cadre de ma formation O'clock comme projet de fin d'étude.
+          <Link exact to="https://notabebe.netlify.app/">lien du projet</Link>
+        </p>
+      </section>
 
     <div className="container">
       <section>
@@ -23,6 +39,7 @@ const Projets = () => (
           Api de bibliothèque personnelle faite en NodeJS et PostgreSQL. Début de front en EJS, mon but est de faire le front en React. (en cours)
         </p>
       </section>
+
       
       <section>
         <h3> Projet à venir </h3>
